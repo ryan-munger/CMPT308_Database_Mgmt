a9e5f264c1e3b9f50666e1fa9f9ad694fac29da5
ryan-munger <ryanmunger10@gmail.com>
2025-02-11 16:59:52 -0500

    Lab 4 complete

--- Lab4/queries.sql
+++ Lab4/queries.sql
@@ -1,29 +1,8 @@
-# Database Lab Report 4
+-- DB Lab 4 Queries
+-- Mine followed by AI
+-- Writeup available.
 
-**Course:** Database Management\
-**Lab Number:** *Lab #4*\
-**Date:** *2025-02-11*\
-**Name:** *Ryan Munger*
 
----
-
-## 1. Objective
-
-*To write some interesting SQL queries using subqueries and set operations. Please do
-not use joins; save them for the next assignment!*
-
-## 2. Lab Setup
-
-*A fresh CAP database.*
-
-## 3. Procedure
-
-### Part 1: *Subqueries*
-
-*Use subqueries to answer the following questions. Then, use AI and grade its responses.*
-
-1. Get all the People data for people who are customers.
-```sql
 -- Me
 SELECT * 
 FROM People
@@ -33,11 +12,7 @@ WHERE pid IN (SELECT pid
 SELECT *
 FROM People
 WHERE pid IN (SELECT pid FROM Customers);
-```
-Grade: 9.9/10: No indents!
 
-2. Get all the People data for people who are agents.
-```sql
 -- Me
 SELECT * 
 FROM People
@@ -47,11 +22,7 @@ WHERE pid IN (SELECT pid
 SELECT *
 FROM People
 WHERE pid IN (SELECT pid FROM Agents);
-```
-Grade 9.9/10
 
-3. Get all of People data for people who are both customers and agents.
-```sql
 -- Me - queryception, I know I could have used AND
 SELECT * 
 FROM People
@@ -64,11 +35,7 @@ SELECT *
 FROM People
 WHERE pid IN (SELECT pid FROM Customers)
   AND pid IN (SELECT pid FROM Agents);
-```
-Grade: 10/10 but I had more fun.
 
-4. Get all of People data for people who are neither customers nor agents.
-```sql
 -- Me
 SELECT * 
 FROM People
@@ -79,11 +46,7 @@ SELECT *
 FROM People
 WHERE pid NOT IN (SELECT pid FROM Customers)
   AND pid NOT IN (SELECT pid FROM Agents);
-```
-Grade 10/10: ditto
 
-5. Get the ID of customers who ordered either product p01 or p03 (or both). List the IDs in order from lowest to highest. Include each ID only once.
-```sql
 -- Me
 SELECT DISTINCT custId
 FROM Orders
@@ -94,11 +57,7 @@ SELECT DISTINCT custId
 FROM Orders
 WHERE prodId = 'p01' OR prodId = 'p03'
 ORDER BY custId ASC;
-```
-Grade: 10/10, as good as human
 
-6. Get the ID of customers who ordered both products p01 and p03. List the IDs in order from highest to lowest. Include each ID only once.
-```sql
 -- Me
 SELECT DISTINCT custId
 FROM Orders
@@ -118,11 +77,7 @@ SELECT custId
 FROM Orders
 WHERE prodId = 'p03'
 ORDER BY custId DESC;
-```
-Grade: 9/10 I would have preferred to see a subquery
 
-7. Get the first and last names of agents who sold products p05 or p07 in order by last name from A to Z.
-```sql
 -- Me
 SELECT firstName, lastName
 FROM People
@@ -137,11 +92,7 @@ SELECT firstName, lastName
 FROM People
 WHERE pid IN (SELECT agentId FROM Orders WHERE prodId = 'p05' OR prodId = 'p07')
 ORDER BY lastName ASC;
-```
-Grade: 9.9/10 where are Alan's indents?
 
-8. Get the home city and birthday of agents booking an order for the customer whose pid is 007, sorted by home city from Z to A.
-```sql
 -- Me
 SELECT homeCity, DOB
 FROM People
@@ -156,11 +107,7 @@ SELECT homeCity, DOB
 FROM People
 WHERE pid IN (SELECT agentId FROM Orders WHERE custId = '007')
 ORDER BY homeCity DESC;
-```
-Grade: 10/10: It did better than me as I did not need to go through the agents table.
 
-9. Get the unique ids of products ordered through any agent who takes at least one order from a customer in Saginaw, sorted by id from highest to lowest. (This is not the same as asking for ids of products ordered by customers in Saginaw.)
-```sql
 -- Me
 SELECT DISTINCT prodId
 FROM Orders
@@ -175,11 +122,7 @@ SELECT DISTINCT prodId
 FROM Orders
 WHERE agentId IN (SELECT pid FROM Agents WHERE pid IN (SELECT agentId FROM Orders WHERE custId IN (SELECT pid FROM People WHERE homeCity = 'Saginaw')))
 ORDER BY prodId DESC;
-```
-Grade: 10/10: So it goes through the Agents table as soon as I don't?
 
-10. Get the last name and home city for all customers who place orders through agents in Regina or Pinner in order by last name from A to Z.
-```sql
 -- Me
 SELECT lastName, homeCity
 FROM People
@@ -196,7 +139,4 @@ SELECT lastName, homeCity
 FROM People
 WHERE pid IN (SELECT custId FROM Orders WHERE agentId IN (SELECT pid FROM Agents WHERE pid IN (SELECT pid FROM People WHERE homeCity = 'Regina' OR homeCity = 'Pinner')))
 ORDER BY lastName ASC;
-```
-Grade: We both had unnecessary subqueries upon closer inspection. That's one long WHERE line though...
 
-Overall Grade: 10/10. I am surprised that a generally clowned on model like Gemini is performing so well even as the queries become more complex.
